995d033ea2632029cc7cbe700d25fc894ae917b5
sushil <sushil@gmail.com>
2023-05-18 18:14:22 +0530

    pussing all files

---
+++ frontend/langauges/ts/datatype/number/string/string.js
@@ -0,0 +1,8 @@
+var name5 = "sushil";
+var name1 = 'sushil';
+var name3 = "sushil";
+var name4 = new String("sushil");
+console.log(name4);
+var gender = 'male';
+console.log(name4, gender);
+console.log(name5.toUpperCase);
---
+++ frontend/langauges/ts/datatype/number/string/string.ts
@@ -0,0 +1,10 @@
+let name5="sushil"
+let name1='sushil';
+let name3=`sushil`;
+let name4= new String("sushil")
+console.log(name4);
+let gender='male';
+console.log(name4,gender);
+console.log(name1.toUpperCase);
+
+
